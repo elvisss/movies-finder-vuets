847394bd2e4f6cc1621ea26e850d5699a9cf629d
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-04-21 00:35:41 -0500

    initital commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'comma-dangle': 'off',
+    'react/prop-types': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Welcome</h1>
+  </main>
+</template>
+
+<style scoped>
+</style>
